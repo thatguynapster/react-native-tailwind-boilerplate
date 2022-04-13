b4306c61e4fcea5c95f65cf4a0705db8984db8d4
Andrew Osei-Wusu <andrew@rootsdigital.tech>
2022-04-13 22:28:43 +0000

    complete setup with supported classes working well

--- App.tsx
+++ App.tsx
@@ -27,40 +27,11 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-// import tw from 'twrnc';
-import tw from './lib/tailwind';
-import {useDeviceContext} from 'twrnc';
-
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import {TailwindProvider} from 'tailwind-rn';
+import MyComponent from './components/my-component';
+import utilities from './tailwind.json';
 
 const App = () => {
-  useDeviceContext(tw); // <- 👋
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -68,33 +39,9 @@ const App = () => {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={tw`text-blue-hover`}>App.tsx</Text> to change
-            this screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <TailwindProvider utilities={utilities}>
+      <MyComponent />
+    </TailwindProvider>
   );
 };
 
--- README.md
+++
@@ -1,2 +0,0 @@
-# react-native-tailwind-boilerplate
-Boilerplate for starting a react-native project with tailwindcss
